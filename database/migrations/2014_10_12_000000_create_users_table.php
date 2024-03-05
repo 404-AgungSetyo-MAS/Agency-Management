495fd47dd5f54059fe7c77da235d793ac790203d
AgungSetyo <ben10agung@gmail.com>
2024-03-05 16:08:19 +0700

    Admin Panel + seed user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username')->unique();
-            $table->string('role');
+            $table->string('name')->unique();
+            $table->string('email')->unique();
+            $table->string('role')->nullable();
             $table->string('password');
-            // $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
