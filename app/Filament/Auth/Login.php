d79b1e13dc9dfe1fc9e9da9226b913ff329ba01c
AgungSetyo <ben10agung@gmail.com>
2024-03-06 08:40:44 +0700

    Login with name or email

---
+++ app/Filament/Auth/Login.php
@@ -0,0 +1,28 @@
+<?php
+
+use Filament\Forms\Form;
+use Filament\Forms\Components\Component;
+use Filament\Forms\Components\TextInput;
+use Filament\Pages\Auth\Login as BaseAuth;
+
+class Login extends BaseAuth
+{
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                $this->getLoginFormComponent(),
+                $this->getPasswordFormComponent(),
+                $this->getRememberFormComponent(),
+            ])
+            ->statePath('data');
+    }
+
+    protected function getLoginFormComponent(): Component
+    {
+        return TextInput::make('login')
+        ->label('Username or Email')
+        ->required()
+        ->autofocus();
+    }
+}
