9c7bb612e16636207ca4029a23017417bcb96280
AgungSetyo <ben10agung@gmail.com>
2024-04-05 10:01:54 +0700

    install Akaunting

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -121,4 +121,5 @@ return array(
     'BladeUI\\Heroicons\\' => array($vendorDir . '/blade-ui-kit/blade-heroicons/src'),
     'App\\' => array($baseDir . '/app', $vendorDir . '/laravel/pint/app'),
     'AnourValar\\EloquentSerialize\\' => array($vendorDir . '/anourvalar/eloquent-serialize/src'),
+    'Akaunting\\Money\\' => array($vendorDir . '/akaunting/laravel-money/src'),
 );
